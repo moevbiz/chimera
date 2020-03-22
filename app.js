5ca49b6f0c5e20c832c51d04f4d5e4a2c52503ec
Bruno Mokross <bruno.mokross@gmail.com>
2020-03-22 15:46:56 +0100

    filtering query

--- app.js
+++ app.js
@@ -1,40 +1,49 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
+app.use(bodyParser.json());
 const router = express.Router();
 const Arena = require('are.na');
 let arena = new Arena();
 
-const channel = 'think-about-it-domjvhshxsq'
+// var channel = 'symbolic-space'
+var channel = 'ittg_p'
 
-// base API route
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// * base API route
 app.get('/api', (req, res) => {
+  channel = req.query.channel ? req.query.channel : channel
   arena.channel(channel).get({ 
     page: req.query.page ? req.query.page : 1, 
     per: req.query.per ? req.query.per : 64,
   direction: 'desc',
   sort: 'position'
   })
-  .then(chan => {
-      res.send(chan);
+  .then(channel => {
+      res.send(channel);
   })
   .catch(err => console.log(err));
 });
 
-app.get('/api/class/:slug', (req, res) => {
-  arena.channel(channel)
-  .search(slug)
-  .get({ page: req.query.page ? req.query.page : 1, per: 64,
-  direction: 'desc',
-  sort: 'position'
-  })
-  .then(chan => {
-      res.send(chan);
-  })
-  .catch(err => console.log(err));
-});
+// * TODO: make single block view work correctly
+// ------------------------------------------------------------------
+// app.get('/api/block/', (req, res) => {
+//   arena.block(req.query.id)
+//   .get()
+//   .then(block => {
+//     res.send(block)
+//   })
+//   .catch(err => console.log(err))
+// })
+// ------------------------------------------------------------------
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
+})
+
+app.get('/block', (req, res) => {
+  res.sendFile(__dirname + '/public/post.html');
 })
 
 app.use(express.static(__dirname + '/public', {
